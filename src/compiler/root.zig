e4b2ea18ef7f2413514d127a4f97e1f6e5f3f46b
masnyjimmy <kspark470@gmail.com>
2026-07-07 19:37:26 +0200

    feat: remove compiling functions from compiler.root

--- src/compiler/root.zig
+++ src/compiler/root.zig
@@ -29,29 +29,3 @@ pub const binary = @import("binary.zig");
 pub const functions = @import("functions.zig");
 
 pub const Workspace = @import("Workspace.zig");
-
-pub fn parse(allocator: std.mem.Allocator, source: lib.source_file.SourceView, diagnostics: *lib.Diagnostic.List) !Parser.Result {
-    var lexer = Lexer.init(source);
-
-    var parser = try Parser.init(allocator, &lexer, diagnostics);
-
-    return try parser.parseFile();
-}
-
-pub const Result = Sema.Result(ir.File);
-
-pub fn compile(
-    allocator: std.mem.Allocator,
-    source: lib.source_file.SourceView,
-    diagnostics: *lib.Diagnostic.List,
-) !Result {
-    const pr = try parse(allocator, source, diagnostics);
-
-    // ir doesnt hold any reference to ast
-    // it can be safely destroyed after ir is built
-    defer pr.deinit();
-
-    var sema = try Sema.init(allocator, diagnostics);
-
-    return try sema.analyse(pr.result);
-}
