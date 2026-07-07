5ebf57123768f3dc6ffb8c1389d3a14818de035b
masnyjimmy <kspark470@gmail.com>
2026-07-07 02:44:29 +0200

    feat: add basic lsp dispatcher implementation

---
+++ src/lsp/root.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+const Dispatcher = @import("dispatcher.zig");
+const lsp = @import("lsp_kit");
+
+pub fn run(
+    allocator: std.mem.Allocator,
+    io: std.Io,
+) !void {
+    var read_buffer: [256]u8 = undefined;
+
+    var stdio_transport: lsp.Transport.Stdio = .init(&read_buffer, .stdin(), .stdout());
+    const transport: *lsp.Transport = &stdio_transport.transport;
+
+    var dispatcher: Dispatcher = .init(allocator);
+    defer dispatcher.deinit();
+
+    try lsp.basic_server.run(io, allocator, transport, &dispatcher, std.log.err);
+}
