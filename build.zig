1e21ba47b7233abb0d5cd9841947d0912a7d17b1
masnyjimmy <kspark470@gmail.com>
2026-07-07 02:15:59 +0200

    feat: Add lsp module to build.zig

--- build.zig
+++ build.zig
@@ -4,10 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const conzole = b.dependency("conzole", .{
+    // dependencies
+    const conzole_mod = b.dependency("conzole", .{
         .target = target,
         .optimize = optimize,
-    });
+    }).module("conzole");
+
+    const lsp_kit_mod = b.dependency("lsp_kit", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("lsp");
 
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/lib/root.zig"),
@@ -27,10 +33,8 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/interpreter/root.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{
-            .{ .name = "conzole", .module = conzole.module("conzole") },
-        },
     });
+    interpreter_mod.addImport("conzole", conzole_mod);
     interpreter_mod.addImport("compiler", compiler_mod);
     interpreter_mod.addImport("lib", lib_mod);
 
@@ -38,13 +42,21 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/cli//root.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{
-            .{ .name = "conzole", .module = conzole.module("conzole") },
-        },
     });
+    cli_mod.addImport("conzole", conzole_mod);
     cli_mod.addImport("lib", lib_mod);
     cli_mod.addImport("compiler", compiler_mod);
     cli_mod.addImport("interpreter", interpreter_mod);
+
+    const lsp_mod = b.createModule(.{
+        .root_source_file = b.path("src/lsp/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lsp_mod.addImport("conzole", conzole_mod);
+    lsp_mod.addImport("lsp_kit", lsp_kit_mod);
+    lsp_mod.addImport("lib", lib_mod);
+    lsp_mod.addImport("compiler", compiler_mod);
 
     const exe = b.addExecutable(.{
         .name = "tasq",
@@ -58,6 +70,7 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("lib", lib_mod);
     exe.root_module.addImport("compiler", compiler_mod);
     exe.root_module.addImport("cli", cli_mod);
+    exe.root_module.addImport("lsp", lsp_mod);
 
     b.installArtifact(exe);
 
