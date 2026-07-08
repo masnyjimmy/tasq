ec5741ea25869a5ea5f9e415f2cb69fd27c734a8
masnyjimmy <kspark470@gmail.com>
2026-07-08 20:21:00 +0200

    fix: make compiler.Span public

--- src/compiler/root.zig
+++ src/compiler/root.zig
@@ -29,6 +29,6 @@ pub const binary = @import("binary.zig");
 pub const functions = @import("functions.zig");
 
 pub const Diagnostics = @import("Diagnostics.zig");
-const Span = Diagnostics.Span;
+pub const Span = Diagnostics.Span;
 
 pub const Workspace = @import("Workspace.zig");
