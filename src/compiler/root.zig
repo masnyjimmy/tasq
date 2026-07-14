f20b1be5d8e9f3d6ac3040fe9909ff577180180f
masnyjimmy <kspark470@gmail.com>
2026-07-14 12:54:55 +0200

    feat: correct span export

--- src/compiler/root.zig
+++ src/compiler/root.zig
@@ -29,6 +29,6 @@ pub const binary = @import("binary.zig");
 pub const functions = @import("functions.zig");
 
 pub const Diagnostics = @import("Diagnostics.zig");
-pub const Span = Diagnostics.Span;
+pub const Span = @import("span.zig");
 
 pub const Workspace = @import("Workspace.zig");
